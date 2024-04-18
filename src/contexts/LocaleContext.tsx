7be42c324a4997f7da68d641ed83ea4ee05b495d
Augusto Barreira <barreira266@hotmail.com>
2024-04-18 03:15:23 -0300

    Add language change support

--- src/contexts/LocaleContext.tsx
+++ src/contexts/LocaleContext.tsx
@@ -1,7 +1,8 @@
 import { i18n } from "i18next";
 import { TFunction } from "i18next/typescript/t";
-import { createContext, ReactNode, useEffect, useState } from "react"
+import { createContext, ReactNode } from "react"
 import { useTranslation } from 'react-i18next';
+import { Language } from "../components/Language";
 
 type LocaleContextType = {
   t: TFunction<"translation", undefined>
@@ -20,6 +21,7 @@ export function LocaleContextProvider({ children }: LocaleContextProviderProps) 
 
   return (
     <LocaleContext.Provider value={{ t, i18n }}>
+      <Language />
       {children}
     </LocaleContext.Provider>
   )
